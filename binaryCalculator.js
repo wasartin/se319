2b21bfd1b3b67341ca34dd56adef3d188c4c9430
wasartin@iastate.edu <wasartin@iastate.edu>
2018-10-07 13:34:36 -0500

    Implemented <<, >>, &, ~, |

--- binaryCalculator.js
+++ binaryCalculator.js
@@ -1,9 +1,13 @@
 var rs = require('readline-sync');
 
 var input1 = rs.question('1st Number: ');
-var input2 = rs.question('2nd Number: ');
-var action = rs.question('Enter the action{+,-,*,/,%}');
-var result;
+var action = rs.question('Enter the action{+,-,*,/,%,<<,>>,&,|,~}: ');
+var input2;
+//?? Shouldn't run
+if(action != '<<' || action != '>>' || action != '~'){
+  input2 = rs.question('2nd Number: ');
+}
+var decimalResult = '';
 
 switch(action){
   case '+':
@@ -13,24 +17,52 @@ switch(action){
   case '%':
     var one = parseInt(input1, 2);
     var two = parseInt(input2, 2);
-    result = one + action + two;
-    result = eval(result);
+    decimalResult = one + action + two;
+    decimalResult = eval(decimalResult);
     break;
   //bit shift left 101 << 1010
   case '<<':
+    decimalResult = input1;
+    decimalResult += '0';
     break;
   //bit shift right 101 >> 10
   case '>>':
+    decimalResult = input1.substring(0, input1.length - 1);
     break;
   //And 1010 & 1000 = 1000
   case '&':
+    var i;
+    for(i = 0; i < input1.length; i++){
+      if(input1[i] == input2[i]){
+        decimalResult += input1[i];
+      }else{
+        decimalResult += '0';
+      }
+    }
     break;
   //Or 1010 | 0101 = 1111
   case '|':
+    var j;
+    for(j = 0; j < input1.length; j++){
+      if(input1[j] == '1' || input2[j] == '1'){
+        decimalResult += '1';
+      }else {
+        decimalResult += '0';
+      }
+    }
     break;
   //invert each bit
   case '~':
+    var i;
+    for(i = 0; i < input1.length; i++){
+      if(input1[i] == '1'){
+        decimalResult += '0';
+      }else {
+        decimalResult += '1';
+      }
+    }
     break;
 }
 
-console.log(result);
+var binaryResult = decimalResult.toString(2);
+console.log('binary: ' + binaryResult + ', decimal: ' + decimalResult);
